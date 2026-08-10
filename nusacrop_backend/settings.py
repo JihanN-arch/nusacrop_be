e73c611f9c73e6c6336a1f65cbfe504980fee057
JihanN-arch <jnpermata07@gmail.com>
2026-08-10 20:28:13 +0700

    nambahin domain vercel

--- nusacrop_backend/settings.py
+++ nusacrop_backend/settings.py
@@ -63,7 +63,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3004",
-    "https://gemastik-softdev-burhan.vercel.app"
+    "https://gemastik-softdev-burhan.vercel.app",
+    "https://nusa-crop-gemastik.vercel.app/",
 ]
 
 from corsheaders.defaults import default_headers
